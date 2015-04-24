bbad913fa4db4abcfde5a7bcc61d8fff739793cd
Shuyang Li <shuyang.li.95@gmail.com>
2015-04-24 11:19:04 -0400

    Add boilerplate twisted server code.

--- castle_server.py
+++ castle_server.py
@@ -1,3 +1,33 @@
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet.endpoints import TCP4ServerEndpoint
+import twisted.internet.reactor as reactor
+
+
+class CastleServerProtocol(Protocol):
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        pass
+
+    def connectionLost(self, reason):
+        pass
+
+    def dataReceived(self, data):
+        # Receive a command from the client
+        pass
+
+
+class CastleServerProtocolFactory(Factory):
+    def __init__(self, castle_server):
+        super(CastleServerProtocolFactory, self).__init__()
+        self.castle_server = castle_server
+
+    def buildProtocol(self, ipv4addr):
+        new_protocol = CastleServerProtocol(self)
+        return new_protocol
+
+
 class CastleServer(object):
     def __init__(self, port, debug=False):
         super(CastleServer, self).__init__()
@@ -6,4 +36,7 @@ class CastleServer(object):
 
     def start():
         # Start listening
-        pass
+        server_protocol_factory = CastleServerProtocolFactory(self)
+        endpoint = TCP4ServerEndpoint(reactor, self.port)
+        endpoint.listen(server_protocol_factory)
+        reactor.run()
